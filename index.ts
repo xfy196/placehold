df3b61b78b2b1c11680fbad97601ce89050377a6
许富荧 <xfy196@outlook.com>
2024-10-27 00:34:41 +0800

    update

--- index.ts
+++ index.ts
@@ -8,14 +8,14 @@ app.get("/:param", async (c: Context) => {
   const { param } = c.req.param();
   let match = null;
   // 判断 param是否为数字
-  match = param.match(/^(\d+)(\.png)?$/);
+  match = param.match(/^(\d+)\.(\w+)?$/);
   if (match) {
-    const [, width, ext = ".png"] = match;
+    const [, width, ext = "png"] = match;
     let newWidth = Number(width) > 4000 ? 4000 : Number(width);
     const buffer = await generatePlaceholdImg({
       width: newWidth,
       height: newWidth,
-      type: ext.replace(".", "") as keyof FormatEnum | AvailableFormatInfo,
+      type: ext as keyof FormatEnum | AvailableFormatInfo,
     })
     const mimeType = mime.getType(ext)
     if(!mimeType){
@@ -25,10 +25,22 @@ app.get("/:param", async (c: Context) => {
     return c.body(buffer)
   }
   // // 不是数字判断分割符号是否存在
-  match = param.match(/^(\d+)x(\d+)(?:\.png)$/);
+  match = param.match(/(\d+)x(\d+)/);
   if (match) {
-    const [, width, height, ext] = match;
-    return c.text(`${width}px x ${height}px`);
+    const [, width, height, ext = 'png'] = match;
+    let newWidth = Number(width) > 4000 ? 4000 : Number(width);
+    let newHeight = Number(height) > 4000 ? 4000 : Number(height);
+    const buffer = await generatePlaceholdImg({
+      width: newWidth,
+      height: newHeight,
+      type: ext.replace(".", "") as keyof FormatEnum | AvailableFormatInfo,
+    })
+    const mimeType = mime.getType(ext)
+    if(!mimeType){
+      return 
+    }
+    c.header("Content-Type", mimeType)
+    return c.body(buffer)
   }
   return c.text("Hello Hono!");
 });
@@ -62,7 +74,7 @@ async function generatePlaceholdImg({
   const svgText = `
     <svg width="${width}" height="${height}">
       <text x="50%" y="50%" dy=".3em" text-anchor="middle" font-size="${fontSize}" font-family="Arial" fill="${textColor}">
-        ${width}x${height}
+        ${width} x ${height}
       </text>
     </svg>
   `
