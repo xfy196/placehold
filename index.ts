b91c20f4639750a281ccfa289a432cbf983f19be
许富荧 <xfy196@outlook.com>
2024-10-27 11:09:38 +0800

    update

--- index.ts
+++ index.ts
@@ -1,9 +1,17 @@
 import { serve } from "@hono/node-server";
 import { Hono, type Context } from "hono";
 import sharp, { type AvailableFormatInfo, type FormatEnum } from "sharp";
-import mime from "mime"
+import mime from "mime";
 const app = new Hono().basePath("/api");
-
+interface ImageInfo  {
+  width: number;
+  height: number;
+  textColor?: string;
+  bgColor?: string;
+  ext?: string;
+  type?: keyof FormatEnum | AvailableFormatInfo;
+  is404?: boolean;
+}
 app.get("/:param", async (c: Context) => {
   const { param } = c.req.param();
   let match = null;
@@ -11,36 +19,21 @@ app.get("/:param", async (c: Context) => {
   match = param.match(/^(\d+)\.(\w+)?$/);
   if (match) {
     const [, width, ext = "png"] = match;
-    let newWidth = Number(width) > 4000 ? 4000 : Number(width);
-    const buffer = await generatePlaceholdImg({
-      width: newWidth,
-      height: newWidth,
-      type: ext as keyof FormatEnum | AvailableFormatInfo,
-    })
-    const mimeType = mime.getType(ext)
-    if(!mimeType){
-      return 
-    }
-    c.header("Content-Type", mimeType)
-    return c.body(buffer)
+   return await sendImageBuffer({
+    width: Number(width),
+    height: Number(width),
+    ext
+   }, c)
   }
   // // 不是数字判断分割符号是否存在
-  match = param.match(/(\d+)x(\d+)/);
+  match = param.match(/^(\d+)x(\d+)\.(\w+)?$/);
   if (match) {
-    const [, width, height, ext = 'png'] = match;
-    let newWidth = Number(width) > 4000 ? 4000 : Number(width);
-    let newHeight = Number(height) > 4000 ? 4000 : Number(height);
-    const buffer = await generatePlaceholdImg({
-      width: newWidth,
-      height: newHeight,
-      type: ext.replace(".", "") as keyof FormatEnum | AvailableFormatInfo,
-    })
-    const mimeType = mime.getType(ext)
-    if(!mimeType){
-      return 
-    }
-    c.header("Content-Type", mimeType)
-    return c.body(buffer)
+    const [, width, height, ext = "png"] = match;
+    return await sendImageBuffer({
+      width: Number(width),
+      height: Number(height),
+      ext
+    }, c)
   }
   return c.text("Hello Hono!");
 });
@@ -54,35 +47,51 @@ async function generatePlaceholdImg({
   textColor = "#999999",
   bgColor = "#dddddd",
   type = "png",
-}: {
-  width: number;
-  height: number;
-  textColor?: string;
-  bgColor?: string;
-  type?: keyof FormatEnum | AvailableFormatInfo;
-}): Promise<Buffer> {
+  is404 = false,
+}: ImageInfo): Promise<Buffer> {
   const image = sharp({
     create: {
       width: width,
       height: height,
       channels: 3,
-      background: bgColor
-    }
-  })
+      background: bgColor,
+    },
+  });
   // 使用SVG叠加文字
-  const fontSize = Math.min(width, height) / 6
+  const fontSize = Math.min(width, height) / 6;
   const svgText = `
     <svg width="${width}" height="${height}">
       <text x="50%" y="50%" dy=".3em" text-anchor="middle" font-size="${fontSize}" font-family="Arial" fill="${textColor}">
-        ${width} x ${height}
+         ${is404 ? "404" : width + " x " + height}
       </text>
     </svg>
-  `
+  `;
   const buffer = await image
-  .composite([{ input: Buffer.from(svgText), gravity: 'center' }])
-  .toFormat(type)
-  .toBuffer()
+    .composite([{ input: Buffer.from(svgText), gravity: "center" }])
+    .toFormat(type)
+    .toBuffer();
   return buffer;
+}
+async function sendImageBuffer({
+  width,
+  height,
+  textColor = "#999999",
+  bgColor = "#dddddd",
+  ext = "png",
+}: ImageInfo, c: Context): Promise<Response>{
+  let newWidth = Number(width) > 4000 ? 4000 : Number(width);
+  let newHeight = Number(height) > 4000 ? 4000 : Number(height);
+  const mimeType = mime.getType(ext);
+  const buffer = await generatePlaceholdImg({
+    width: newWidth,
+    height: newHeight,
+    textColor,
+    bgColor,
+    type: ext.replace(".", "") as keyof FormatEnum | AvailableFormatInfo,
+    is404: !mimeType
+  });
+  c.header("Content-Type", mimeType ?? 'image/png');
+  return c.body(buffer)
 }
 
 serve({
